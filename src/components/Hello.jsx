24ea54be6196ae7d652d7975ff6f844d0bb352ea
Your Name <digneli14@gmail.com>
2020-06-28 04:11:38 +0200

    arreglando proyecto en otro equipo

---
+++ src/components/Hello.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Hello = () => (
+    <div className='carousel'>
+      <div className='carousel-container'>
+       <h1>Hello world</h1>
+      </div>
+    </div>
+  );
+  
+  export default Hello;
