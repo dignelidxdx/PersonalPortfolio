a9efb40c5427671623f570800e7119eb0a853029
Your Name <digneli14@gmail.com>
2020-07-06 01:53:51 +0200

    Trabajando con react Hooks para el manejo de estado con useState y tambien con redux para uso de actions para llevar del submit login al home

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,41 +1,86 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { loginRequest } from '../actions';
 import logo from '../assets/static/logo.png';
 import twitter from '../assets/static/twitter-icon.png';
 import google from '../assets/static/google-icon.png';
 import '../assets/styles/components/Login.scss';
 
-const Login = () => (
-    <section className="login">
-    <section className="login-container">
-    <img src={logo} alt="logo-digneli"/>
-        <h2>Inicia sesión</h2>
-        <form className="login-container__form" action="">
-            <input className="input" type="text" placeholder="Correo" />
-            <input className="input" type="password" name="password" id="password" placeholder="Contraseña" />
+const Login = (props) => { 
 
-            <label for="password">
-                <input id="mostrar_contrasena" type="checkbox" name="" id="cbox1" value="checkbox" />Mostrar Contraseña
-            </label>
-            <br/>
-            <button className="button">Iniciar sesión</button>
-            <div className="login-container__remember">
-                <label for="">
-                    <input type="checkbox" name="" id="cbox1" value="checkbox" />Recuerdame
+    const [form, setValues] = useState({
+        email: '',
+    });
+
+    const handleInput = event => {
+        setValues({
+            ...form,
+            [event.target.name]: event.target.value
+        })
+    }
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        props.loginRequest(form);
+        props.history.push('/');
+    }
+    
+    return (
+        <section className="login">
+        <section className="login-container">
+        <img src={logo} alt="logo-digneli"/>
+            <h2>Inicia sesión</h2>
+            <form className="login-container__form" action="" onSubmit={handleSubmit}>
+                <input 
+                    name="email"
+                    className="input" 
+                    type="text" 
+                    placeholder="Correo"
+                    onChange={handleInput}
+                    />
+                <input 
+                    name="password" 
+                    className="input" 
+                    type="password" 
+                    id="password" 
+                    placeholder="Contraseña" 
+                    onChange={handleInput}
+                    />
+
+                <label for="password">
+                    <input 
+                    id="mostrar_contrasena" 
+                    type="checkbox" 
+                    name="" 
+                    id="cbox1" 
+                    value="checkbox" />Mostrar Contraseña
                 </label>
-                <a href="">Olvidé mi contraseña</a>
-            </div>
-        </form>
-        <section className="login-container__social">
-            <div><img width="20px" src={google} alt="" />Inicia sesión con Google</div>
-            <div><img width="20px" src={twitter} alt="" />Inicia sesión con Twitter</div>
+                <br/>
+                <button className="button">Iniciar sesión</button>
+                <div className="login-container__remember">
+                    <label for="">
+                        <input type="checkbox" name="" id="cbox1" value="checkbox" />Recuerdame
+                    </label>
+                    <a href="">Olvidé mi contraseña</a>
+                </div>
+            </form>
+            <section className="login-container__social">
+                <div><img width="20px" src={google} alt="" />Inicia sesión con Google</div>
+                <div><img width="20px" src={twitter} alt="" />Inicia sesión con Twitter</div>
+            </section>
+            <p className="login-container__register">No tienes ninguna cuenta <Link to='/register'>Regístrate</Link> </p>
         </section>
-        <p className="login-container__register">No tienes ninguna cuenta <Link to='/register'>Regístrate</Link> </p>
     </section>
-</section>
 
-);
+    )
 
-export default Login;
+};
+
+const mapDispatchToProps = {
+    loginRequest,
+}
+
+export default connect(null, mapDispatchToProps)(Login);
 
