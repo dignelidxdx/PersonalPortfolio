ef2d3dc740b4aec9e69bd2e2ef4e54d151698248
dignelidxdx <digneli14@gmail.com>
2025-08-20 22:43:42 -0300

    change link documents to present

--- src/components/GeneratePDF.jsx
+++ src/components/GeneratePDF.jsx
@@ -12,7 +12,7 @@ class GeneratePDF extends PureComponent {
           {' '}
           <p>Para descargar mi CV formato pdf</p>
           {' '}
-          <a href='https://firebasestorage.googleapis.com/v0/b/my-portfolio-2a204.appspot.com/o/CV-Digneli-Davila-Spanish.pdf?alt=media&token=bf16bd47-1450-48b9-81f6-98673066a2aa' download>
+          <a href='https://firebasestorage.googleapis.com/v0/b/my-portfolio-2a204.appspot.com/o/CV-spanish.pdf?alt=media&token=13677d00-80e9-4290-a0f6-45ddab1b69b6' download>
             {' '}
             <AiOutlineFilePdf />
             {' '}
@@ -20,7 +20,7 @@ class GeneratePDF extends PureComponent {
           </a>
           {' '}
           {' '}
-          <a href='https://firebasestorage.googleapis.com/v0/b/my-portfolio-2a204.appspot.com/o/CV-Digneli-Davila-English.pdf?alt=media&token=6a8ef4f6-a683-4169-9404-29ca9a1f35d7' download>
+          <a href='https://firebasestorage.googleapis.com/v0/b/my-portfolio-2a204.appspot.com/o/CV-english.pdf?alt=media&token=826ade25-c18d-4f3c-aebf-97e720294301' download>
             <AiOutlineFilePdf />
             {' '}
             En inglés
